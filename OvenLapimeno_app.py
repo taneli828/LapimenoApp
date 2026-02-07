da23a8adbf2930cfef478b983a0c482247bc678f
taneli828 <taneli828@gmail.com>
2026-02-07 22:33:34 +0200

    Update OvenLapimeno_app.py

--- OvenLapimeno_app.py
+++ OvenLapimeno_app.py
@@ -2,9 +2,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime, time, timedelta
-git add requirements.txt.txt
-git commit -m "Lisätty requirements.txt.txt kirjastoille"
-git push origin main
 # --- Sovelluksen otsikko ---
 st.set_page_config(page_title="Oven läpimenoanalyysi", page_icon="🏭")
 st.title("Oven läpimenoanalyysi")
@@ -107,5 +104,6 @@ if tiedosto:
         if st.button("Tallenna Exceliin"):
             df_valittu.to_excel(tiedostonimi, index=False)
             st.success(f"Tulokset tallennettu tiedostoon: {tiedostonimi}")
+
 
 
